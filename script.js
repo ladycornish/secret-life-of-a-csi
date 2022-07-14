2e7221301ae865094635281040fd5077ebde895a
Mel <melcornish@me.com>
2022-07-14 11:56:11 +0100

    add download button

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 const nav = document.querySelector(".nav");
-const btn = document.querySelector('.hamburger');
+const btn = document.querySelector(".hamburger");
 
-btn.addEventListener('click', () => {
-    nav.classList.toggle('visible')
+btn.addEventListener("click", () => {
+  nav.classList.toggle("visible");
 
-    setTimeout(function () {
-        document.querySelector('.nav').style.display = 'none'
-    }, 5000)
-    setTimeout("location.reload(true);", 5000);
-})
+  setTimeout(function () {
+    document.querySelector(".nav").style.display = "none";
+  }, 5000);
+  setTimeout("location.reload(true);", 5000);
+});
 
+const passwd = () => {
+  const password = prompt("Enter The password to download the file: ");
+
+  if (password.toLowerCase() == "booksample") {
+    window.open("test-sample.pdf");
+  } else {
+    alert("Incorrent Password!! Please try Again!");
+  }
+};
