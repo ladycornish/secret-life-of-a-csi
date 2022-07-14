ad653517ca7c584a4fc3489662b52d44a9d438c3
Mel <melcornish@me.com>
2022-07-14 14:53:41 +0100

    re-do nav bar on all pages

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
-const nav = document.querySelector(".nav");
-const btn = document.querySelector(".hamburger");
+const menu = document.querySelector(".nav-list");
+const menuWrapper = document.querySelector("nav-wrapper");
+const hamburger = document.querySelector(".nav-toggle");
+const closeBtn = document.querySelector(".close");
 
-btn.addEventListener("click", () => {
-  nav.classList.toggle("visible");
+const showMenu = () => {
+  hamburger.style.display = "none";
+  closeBtn.style.transform = "translateY(0)";
+  menu.style.transform = "translateX(0)";
+  menuWrapper.style.transform = "translateX(0)";
+};
 
-  setTimeout(function () {
-    document.querySelector(".nav").style.display = "none";
-  }, 5000);
-  setTimeout("location.reload(true);", 5000);
-});
+const hideMenu = () => {
+  closeBtn.style.transform = "translateY(-20rem)";
+  hamburger.style.display = "block";
+  menu.style.transform = "translateX(200%)";
+  menuWrapper.style.transform = "translateX(-200%)";
+};
+
+hamburger.addEventListener("click", showMenu);
+closeBtn.addEventListener("click", hideMenu);
+menuWrapper.addEventListener("click", hideMenu);
 
 const passwd = () => {
   const password = prompt("Enter The password to download the file: ");
